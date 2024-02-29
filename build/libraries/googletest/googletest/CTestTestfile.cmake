77fbf37e1882f5c30a629678d7af955cab4c760a
Данила <131337466+shaman1641@users.noreply.github.com>
2024-02-29 20:09:30 +0700

    Delete build/libraries directory

--- build/libraries/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shaman1641/HDD/Github_repository/Homework_upprpo/libraries/googletest/googletest
-# Build directory: /home/shaman1641/HDD/Github_repository/Homework_upprpo/build/libraries/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
