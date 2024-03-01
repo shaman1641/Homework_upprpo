9737453d2058d069d39108b6dc812ac75aa04aed
Данила <shaman1641@mail.ru>
2024-03-01 10:08:38 +0700

    -cmake

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shaman1641/HDD/Github_repository/Homework_upprpo
-# Build directory: /home/shaman1641/HDD/Github_repository/Homework_upprpo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libraries/googletest")
-subdirs("tests")
