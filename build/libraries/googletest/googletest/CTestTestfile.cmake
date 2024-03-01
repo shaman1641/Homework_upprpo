d062978dc57ec8b3ca22e98897a6015920cf4553
Данила <shaman1641@mail.ru>
2024-03-01 10:01:14 +0700

    Add

---
+++ build/libraries/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shaman1641/HDD/Github_repository/Homework_upprpo/libraries/googletest/googletest
+# Build directory: /home/shaman1641/HDD/Github_repository/Homework_upprpo/build/libraries/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
